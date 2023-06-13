b7119fa99af779f576e2838b27fe7c660c0a90f2
AliHamade28 <106478899+AliHamade28@users.noreply.github.com>
2023-06-13 20:00:46 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter your name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello, {0} the current time is {1}", userName, System.DateTime.Now.TimeOfDay);
+
+            Console.WriteLine("Press any key to quit.");
+            Console.ReadKey();
+
+        }
+    }
+}
